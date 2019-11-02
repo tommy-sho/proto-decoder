7bd18e6927e8c54b95cc16c9db094174a7d812f6
tomiokashougo <tomiokasyogo@gmail.com>
2019-11-03 01:50:11 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tommy-sho/proto-decoder
+
+go 1.13
+
+require github.com/golang/protobuf v1.3.2
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/tommy-sho/proto-decoder/_proto"
+)
+
+func main() {
+	p := &pb.Person{
+		Name: &pb.Name{
+			Value: "Alice",
+		},
+		Age: &pb.Age{
+			Value: 20,
+		},
+	}
+
+	if err := write("./person/alice.bin", p); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func write(file string, p *pb.Person) error {
+	out, err := proto.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %w", err)
+	}
+
+	if err := ioutil.WriteFile(file, out, 0644); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
